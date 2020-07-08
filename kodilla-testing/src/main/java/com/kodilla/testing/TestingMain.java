f4b483a516aad82dad120dafefb513f805e5efa5
lukaszlizewski <lizewski.lukasz@gmail.com>
2020-07-08 22:03:06 +0200

    6_2_Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,24 @@
 package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania.");
+        Calculator calculator = new Calculator();
+        int a = 3;
+        int b = 2;
+        System.out.println("Firs unit test: add A="+a+" to B="+b);
+        System.out.println("Result: "+ calculator.addAToB(a,b));
+        if (a+b==calculator.addAToB(a,b)){
+            System.out.println("Correct");
+        } else {
+            System.out.println("Not correct");
+        }
+        System.out.println("Second unit test: subtract B="+b+" from A="+a);
+        System.out.println("Result: "+ calculator.subtractBFromA(a,b));
+        if (a-b==calculator.subtractBFromA(a,b)){
+            System.out.println("Correct");
+        } else {
+            System.out.println("Not correct");
+        }
     }
 }
