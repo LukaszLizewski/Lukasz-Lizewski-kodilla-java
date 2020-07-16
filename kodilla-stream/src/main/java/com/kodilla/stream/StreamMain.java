02fe6f94b21a25b3d66bd161d928d0014447cc1a
lukaszlizewski <lizewski.lukasz@gmail.com>
2020-07-16 23:47:06 +0200

    7_2_Final

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
 
 import static jdk.nashorn.internal.objects.NativeString.toUpperCase;
@@ -15,25 +16,29 @@ public class StreamMain {
         poemBeautifier.beautify(":) ", "more beautiful text", " (:", (a, b, c) -> toUpperCase(a + b + c));
         poemBeautifier.beautify("abc ", "more beautiful text", " abc", (a, b, c) -> toUpperCase(a + b + c));
         poemBeautifier.beautify(" *** ", "more beautiful text", "", (a, b, c) -> toUpperCase(a + b + a));
-    }
-}
-        /*
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
         System.out.println("Calculating expressions with lambdas:");
-        expressionExecutor.executeExpression(10,5,(a, b) -> a+b);
-        expressionExecutor.executeExpression(10,5,(a, b) -> a-b);
-        expressionExecutor.executeExpression(10,5,(a, b) -> a*b);
-        expressionExecutor.executeExpression(10,5,(a, b) -> a/b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
 
         System.out.println("Calculating expressions with method references");
-        expressionExecutor.executeExpression(3,4,FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3,4,FunctionalCalculator::divideAByB);
-        expressionExecutor.executeExpression(3,4,FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3,4,FunctionalCalculator::subBfromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBfromA);
 
         System.out.println("LICZENIE ZA POMOCĄ METOD STATYCZNYCH NIEMUSIEJĄCYCH TWORZYC WłASNYCH OBIEKTÓW");
-        System.out.println(FunctionalCalculator.addAToB(10,5));
-        System.out.println(FunctionalCalculator.divideAByB(10,5));
-        System.out.println(FunctionalCalculator.multiplyAByB(10,5));
-        System.out.println(FunctionalCalculator.subBfromA(10,5));
-         */
+        System.out.println(FunctionalCalculator.addAToB(10, 5));
+        System.out.println(FunctionalCalculator.divideAByB(10, 5));
+        System.out.println(FunctionalCalculator.multiplyAByB(10, 5));
+        System.out.println(FunctionalCalculator.subBfromA(10, 5));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
+    }
+}
+
 
