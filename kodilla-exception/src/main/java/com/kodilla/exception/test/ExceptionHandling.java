ab8afe553d070b5cd42d827e266ceb66a5e23d64
lukaszlizewski <lizewski.lukasz@gmail.com>
2020-07-22 17:02:13 +0200

    8_3_SecondChallenge

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,4 +1,14 @@
 package com.kodilla.exception.test;
 
 public class ExceptionHandling {
+    public static void main(String args[]){
+        SecondChallenge secondChallenge = new SecondChallenge();
+        try {
+            secondChallenge.probablyIWillThrowException(2.0,1.5);
+        } catch (Exception e){
+            System.out.println("Exception has been caught");
+        } finally {
+            System.out.println("Program is still working");
+        }
+    }
 }
