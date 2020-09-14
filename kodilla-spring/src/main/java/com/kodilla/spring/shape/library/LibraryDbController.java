e47c1c0ae84908622a62121410fa4356c5dcde79
lukaszlizewski <lizewski.lukasz@gmail.com>
2020-09-14 22:16:50 +0200

    modul_14_2

--- kodilla-spring/src/main/java/com/kodilla/spring/shape/library/LibraryDbController.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/shape/library/LibraryDbController.java
@@ -1,4 +1,13 @@
 package com.kodilla.spring.shape.library;
 
-public class LibraryDbController {
+import org.springframework.stereotype.Repository;
+
+@Repository
+public final class LibraryDbController {
+    public void saveData(){
+        System.out.println("Saving data to the database.");
+    }
+    public void loadData(){
+        System.out.println("Loading data from the database.");
+    }
 }
