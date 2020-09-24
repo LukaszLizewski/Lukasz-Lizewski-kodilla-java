dec42d098ffdb62879bc679e7bc8c10103c842a8
lukaszlizewski <lizewski.lukasz@gmail.com>
2020-09-24 17:00:42 +0200

    modul_17_1

--- kodilla-spring/src/test/java/com/kodilla/spring/reader/ReaderTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/reader/ReaderTestSuite.java
@@ -5,14 +5,14 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ReaderTestSuite {
-    @Test
+   /* @Test
     public void testRead(){
         //Given
         ApplicationContext context = new AnnotationConfigApplicationContext(ReaderConfig.class);
         Reader reader = context.getBean(Reader.class);
         //When & Then
         reader.read();
-    }
+    }*/
     @Test
     public void testConditional() {
         //Given
