f063be09cab3eb0dcf04003fb4ceae6f79eaaa56
lukaszlizewski <lizewski.lukasz@gmail.com>
2020-09-28 20:57:55 +0200

    modul_17_4

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/dao/TaskListDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/dao/TaskListDao.java
@@ -2,7 +2,9 @@ package com.kodilla.hibernate.tasklist.dao;
 
 import com.kodilla.hibernate.task.Task;
 import com.kodilla.hibernate.tasklist.TaskList;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import javax.transaction.Transactional;
 import java.util.List;
@@ -11,4 +13,5 @@ import java.util.List;
 @Repository
 public interface TaskListDao extends CrudRepository <TaskList, Integer> {
     List<TaskList> findByListName (String listName);
+
 }
